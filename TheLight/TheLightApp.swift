73c6d214c0bf6466760f18fdd71ac0dd9f45938d
Khulood Alhamed <Khuloodalhamed@gmail.com>
2023-01-08 11:25:42 +0300

    Initial Commit

---
+++ TheLight/TheLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLightApp.swift
+//  TheLight
+//
+//  Created by Khulood Alhamed on 08/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TheLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
